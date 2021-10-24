83a69b54c2a3bdbb89421b0856cdf285394a1409
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-10-24 16:03:56 +0530

    Add model animation, fog and sky

--- src/components/MagicRoom.jsx
+++ src/components/MagicRoom.jsx
@@ -1,31 +1,39 @@
-// eslint-disable
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls, Stars } from '@react-three/drei'
+import { useGLTF, OrbitControls, Stars, useAnimations, Sky } from '@react-three/drei'
 
 const MagicRoom = () => {
   // Importing model
   const Model = () => {
     const gltf = useGLTF('./libs/3_seconds_of_vacations/scene.gltf', true)
+    const { ref, names, actions } = useAnimations(gltf.animations)
+    useEffect(() => {
+    actions[names[0]].play()
+  })
     return (
       <primitive
         object={gltf.scene}
+        ref={ref}
         dispose={null}
         scale={10}
-        position={[0, -12, 0]}
+        rotation={[0, Math.PI * 1.18, 0]}
+        position={[0, -20, 0]}
       />
     )
   }
 
   return (
     <Canvas
-      camera={{ position: [0, 0, 100], fov: 65 }}
+      dpr={[1, 2]}
+      camera={{ position: [0, 0, 100], fov: 45 }}
       style={{ height: '100vh', width: '100%' }}
     >
       <Suspense fallback={null}>
         <OrbitControls enablePan enableZoom enableRotate />
+        <Sky azimuth={1} inclination={0.6} distance={1000} />
+        <fog attach="fog" args={['#ffffff', 0, 300 ]} />
         <Model />
-        <Stars
+          <Stars
           radius={8} // Radius of the inner sphere (default=100)
           depth={50} // Depth of area where stars should fit (default=50)
           count={10000} // Amount of stars (default=5000)
