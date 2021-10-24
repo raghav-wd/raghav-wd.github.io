4de0dc8d41530b1f2b5b415dfa98c521f3d5b668
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-10-24 22:41:32 +0530

    Add built

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,9 @@
+const Overlay = () => (
+        <div className="text">
+        <span>Hi, I&apos;m</span>
+        <br /><br />
+        <span>Raghav</span>
+      </div>
+)
+    
+export default Overlay
