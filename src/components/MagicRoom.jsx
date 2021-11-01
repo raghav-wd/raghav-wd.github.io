b54d9d8fef98ffa1f160a7621bbafff7d48637c0
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-11-01 17:29:07 +0530

    Add seo tags

--- src/components/MagicRoom.jsx
+++ src/components/MagicRoom.jsx
@@ -1,5 +1,6 @@
-import React, { Suspense, useEffect } from 'react'
-import { Canvas } from '@react-three/fiber'
+/* eslint-disable */
+import React, { Suspense, useState, useEffect, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import {
   useGLTF,
   OrbitControls,
@@ -36,20 +37,35 @@ const MagicRoom = () => {
     >
       <Suspense fallback={null}>
         <OrbitControls enablePan enableZoom enableRotate />
-        <Sky azimuth={1} inclination={0.6} distance={1000} />
+        <Sky
+          distance={250}
+          sunPosition={[3, 1, 0]} // Sun position normal (defaults to inclination and azimuth if not set)
+          inclination={0} // Sun elevation angle from 0 to 1 (default=0)
+          azimuth={0.25}/>
         <fog attach="fog" args={['#ffffff', 0, 300]} />
         <Model />
-        <Stars
+        <Rotate>
+          <Stars
           radius={8} // Radius of the inner sphere (default=100)
           depth={50} // Depth of area where stars should fit (default=50)
-          count={10000} // Amount of stars (default=5000)
+          count={50000} // Amount of stars (default=5000)
           factor={2} // Size factor (default=4)
           saturation={0} // Saturation 0-1 (default=0)
           fade // Faded dots (default=false)
         />
+        </Rotate>
       </Suspense>
     </Canvas>
   )
+}
+
+function Rotate(props) {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.x += 0.001
+    ref.current.rotation.y -= 0.002
+  });
+  return <group ref={ref} {...props} />;
 }
 
 export default MagicRoom
