ba576a738920bf6f92f911ff02faddf1aa8be444
raghav-wd <raghavseries572@gmail.com>
2021-11-05 20:21:05 +0530

    Add person camera and wasd movement

--- src/components/Models/Person.jsx
+++ src/components/Models/Person.jsx
@@ -5,13 +5,15 @@ import {
   useGLTF,
   OrbitControls,
   useAnimations,
-  Billboard,
+  PerspectiveCamera,
   Stats,
 } from '@react-three/drei'
 import { GUI } from 'dat.gui'
 
 const Person = () => {
   let panel, personGuiPanel
+  const camera = useRef()
+
   useEffect(() => {
     panel = new GUI()
     personGuiPanel = panel.addFolder('Person')
@@ -24,17 +26,87 @@ const Person = () => {
 
     // console.log(gltf.scene)
     useEffect(() => {
-      console.log(ref)
+      // console.log(ref)
+      document.addEventListener('keydown', function (event) {
+        console.log(
+          `Key: ${event.key} with keycode ${event.keyCode} has been pressed`
+        )
+        switch (event.key) {
+          case 'w': {
+            ref.current.position.z += 0.04
+            break
+          }
+          case 'a': {
+            ref.current.position.x += 0.04
+            break
+          }
+          case 's': {
+            ref.current.position.z -= 0.04
+            break
+          }
+          case 'd': {
+            ref.current.position.x -= 0.04
+            break
+          }
+        }
+      })
       personGuiPanel
         .add(ref.current.position, 'x')
         .min(0)
         .max(5)
         .step(0.1)
         .name('x')
+      personGuiPanel
+        .add(ref.current.position, 'y')
+        .min(0)
+        .max(5)
+        .step(0.1)
+        .name('y')
+      personGuiPanel
+        .add(ref.current.position, 'z')
+        .min(0)
+        .max(5)
+        .step(0.05)
+        .name('z')
       personGuiPanel.add(ref.current, 'visible')
       actions.walk.play()
     })
     return <primitive ref={ref} object={gltf.scene} dispose={null} />
+  }
+
+  function PersonCamera() {
+    const ref = useRef()
+    useEffect(() => {
+      let cameraGuiPanel = panel.addFolder('Camera')
+      console.log(ref.current)
+      cameraGuiPanel
+        .add(ref.current.position, 'x')
+        .min(0)
+        .max(5)
+        .step(0.1)
+        .name('x')
+      cameraGuiPanel
+        .add(ref.current.position, 'y')
+        .min(0)
+        .max(5)
+        .step(0.1)
+        .name('y')
+      cameraGuiPanel
+        .add(ref.current.position, 'z')
+        .min(-5)
+        .max(5)
+        .step(0.05)
+        .name('z')
+    })
+    return (
+      <PerspectiveCamera
+        fov={45}
+        ref={ref}
+        position={[0, 2, -5]}
+        makeDefault // Registers it as the default camera system-wide (default=false)
+        // {...props} // All THREE.PerspectiveCamera props are valid
+      ></PerspectiveCamera>
+    )
   }
 
   return (
@@ -42,11 +114,13 @@ const Person = () => {
       dpr={[1, 2]}
       camera={{ fov: 45 }}
       style={{ height: '100vh', width: '100%' }}
+      onKeyDown={() => console.log('hi')}
     >
       <Stats
         showPanel={0} // Start-up panel (default=0)
         className="stats" // Optional className to add to the stats container dom element
       />
+      <PersonCamera />
       <OrbitControls enablePan enableZoom enableRotate />
       <ambientLight />
       <axesHelper args={[5]} />
