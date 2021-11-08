e392ed176e5c1ad20ad906d329ac1d038f6aae94
raghav-wd <raghav.gupta.gpt@gmail.com>
2021-11-08 12:55:37 +0530

    Syncronized sphere in cannon world with the model for walking

--- src/components/Models/Person.jsx
+++ src/components/Models/Person.jsx
@@ -2,6 +2,14 @@
 import React, { Suspense, useState, useEffect, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import {
+  Physics,
+  useSphere,
+  usePlane,
+  useBox,
+  Debug,
+  useCylinder,
+} from '@react-three/cannon'
+import {
   useGLTF,
   OrbitControls,
   useAnimations,
@@ -13,25 +21,34 @@ import { GUI } from 'dat.gui'
 const Person = () => {
   const blendDuration = 0.4
   let panel, personGuiPanel, cameraGuiPanel
-  const camera = useRef()
 
   useEffect(() => {
     panel = new GUI()
     personGuiPanel = panel.addFolder('Person')
     cameraGuiPanel = panel.addFolder('Camera')
-  })
+  }, [])
 
   // Importing model
   const Model = () => {
     const gltf = useGLTF('./libs/Xbot.glb', '/draco-gltf')
     const { ref, mixer, names, actions, clips } = useAnimations(gltf.animations)
+    const [mesh, api] = useSphere(() => ({
+      mass: 1,
+      position: [0, 1, 0],
+      type: 'Dynamic',
+    }))
     const [currentAction, setCurrentAction] = useState('idle')
 
     useEffect(() => {
-      console.log(currentAction)
       actions[currentAction]?.reset().fadeIn(blendDuration).play()
       return () => void actions[currentAction]?.fadeOut(blendDuration)
     }, [currentAction])
+
+    useFrame(() => {
+      api.velocity.set(0, 0, 1)
+      mesh.current.getWorldPosition(ref.current.position)
+      ref.current.position.y -= 1
+    })
 
     useEffect(() => {
       document.addEventListener('keydown', function (event) {
@@ -82,7 +99,12 @@ const Person = () => {
         personGuiPanel.add(ref.current, 'visible')
       }
     }, [])
-    return <primitive ref={ref} object={gltf.scene} dispose={null} />
+    return (
+      <mesh>
+        <mesh ref={mesh}></mesh>
+        <primitive ref={ref} object={gltf.scene} dispose={null} />
+      </mesh>
+    )
   }
 
   function PersonCamera() {
@@ -115,7 +137,6 @@ const Person = () => {
         ref={ref}
         position={[0, 2, -5]}
         makeDefault // Registers it as the default camera system-wide (default=false)
-        // {...props} // All THREE.PerspectiveCamera props are valid
       ></PerspectiveCamera>
     )
   }
@@ -125,7 +146,6 @@ const Person = () => {
       dpr={[1, 2]}
       camera={{ fov: 45 }}
       style={{ height: '100vh', width: '100%' }}
-      onKeyDown={() => console.log('hi')}
     >
       <Stats
         showPanel={0} // Start-up panel (default=0)
@@ -135,21 +155,26 @@ const Person = () => {
       <OrbitControls enablePan enableZoom enableRotate />
       <ambientLight />
       <axesHelper args={[5]} />
-      <Plane />
-      <Suspense fallback={null}>
-        <Model />
-      </Suspense>
+      {/* <Plane /> */}
+      <Physics>
+        <Debug>
+          <Plane />
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+        </Debug>
+      </Physics>
     </Canvas>
   )
 }
 
 function Plane(props) {
   // This reference will give us direct access to the mesh
-  const mesh = useRef()
+  const [mesh] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
   // console.log(mesh.current.x)
   return (
-    <mesh {...props} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry ref={mesh} args={[100, 100]} />
+    <mesh ref={mesh} {...props} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry args={[100, 100]} />
       <meshStandardMaterial color="orange" />
     </mesh>
   )
