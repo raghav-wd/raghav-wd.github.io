085978041515878ccdf2ab2c32a8c0265c0872c8
raghav-wd <raghav.gupta.gpt@gmail.com>
2021-11-09 17:39:24 +0530

    Add movement animation to model

--- src/components/Models/Person.jsx
+++ src/components/Models/Person.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import React, { Suspense, useState, useEffect, useRef } from 'react'
 import * as THREE from 'three'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import {
   Physics,
   useSphere,
@@ -20,8 +20,9 @@ import {
 import { GUI } from 'dat.gui'
 
 const Person = () => {
-  const blendDuration = 0.4
+  const blendDuration = 0.15
   let panel, personGuiPanel, cameraGuiPanel
+  const [currentAction, setCurrentAction] = useState('walk')
 
   const SPEED = 5
   const direction = new THREE.Vector3()
@@ -36,145 +37,119 @@ const Person = () => {
     cameraGuiPanel = panel.addFolder('Camera')
   }, [])
 
-  const keys = { KeyW: "forward", KeyS: "backward", KeyA: "left", KeyD: "right", Space: "jump" }
+  const keys = {
+    KeyW: 'forward',
+    KeyS: 'backward',
+    KeyA: 'left',
+    KeyD: 'right',
+    Space: 'jump',
+  }
   const moveFieldByKey = (key) => keys[key]
 
   const usePlayerControls = () => {
-  const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false })
-  useEffect(() => {
-    const handleKeyDown = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }))
-    const handleKeyUp = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
-    document.addEventListener("keydown", handleKeyDown)
-    document.addEventListener("keyup", handleKeyUp)
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown)
-      document.removeEventListener("keyup", handleKeyUp)
-    }
-  }, [])
-  return movement
-}
-// Importing model
-const Model = () => {
-  const gltf = useGLTF('./libs/Xbot.glb', '/draco-gltf')
-  const { ref, mixer, names, actions, clips } = useAnimations(gltf.animations)
-  const { forward, backward, left, right, jump } = usePlayerControls()
-  const velocity = useRef([0, 0, 0])
+    const [movement, setMovement] = useState({
+      forward: false,
+      backward: false,
+      left: false,
+      right: false,
+      jump: false,
+    })
+    useEffect(() => {
+      const handleKeyDown = (e) => {
+        setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }))
+      }
+      const handleKeyUp = (e) => {
+        setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
+      }
+      document.addEventListener('keydown', handleKeyDown)
+      document.addEventListener('keyup', handleKeyUp)
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown)
+        document.removeEventListener('keyup', handleKeyUp)
+      }
+    }, [])
+    return movement
+  }
 
-  frontVector.set(0, 0, Number(forward) - Number(backward))
-  sideVector.set(Number(right) - Number(left), 0, 0)
-  direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED)
+  // Importing model
+  const Model = () => {
+    const gltf = useGLTF('./libs/Xbot.glb', '/draco-gltf')
+    const { ref, mixer, names, actions, clips } = useAnimations(gltf.animations)
+    const { forward, backward, left, right, jump } = usePlayerControls()
+    const personCameraRef = useRef(null)
+    const { camera } = useThree()
+    const velocity = useRef([0, 0, 0])
 
     const [mesh, api] = useSphere(() => ({
       mass: 1,
       position: [0, 1, 0],
       type: 'Dynamic',
     }))
-    const [currentAction, setCurrentAction] = useState('idle')
 
-    useEffect(() => api.velocity.subscribe((v) => (velocity.current = v)), [])
-    
     useEffect(() => {
-      actions[currentAction]?.reset().fadeIn(blendDuration).play()
-      return () => void actions[currentAction]?.fadeOut(blendDuration)
-    }, [currentAction])
+      frontVector.set(0, 0, Number(forward) - Number(backward))
+      sideVector.set(Number(right) - Number(left), 0, 0)
+      direction
+        .subVectors(frontVector, sideVector)
+        .normalize()
+        .multiplyScalar(SPEED)
+
+      if (forward || right || left) {
+        actions.idle.reset().fadeOut(blendDuration)
+        actions.walk.reset().fadeIn(blendDuration).play()
+      }
+      return () => {
+        if (forward || right || left) {
+          actions.idle.reset().fadeIn(blendDuration).play()
+          actions.walk.reset().fadeOut(blendDuration)
+        }
+      }
+    }, [forward, backward, right, left])
+
+    useEffect(() => {
+      actions.idle.play()
+      camera.position.set(0, 1.45, -4.25)
+      camera.lookAt(0, 1.45, 0)
+      api.velocity.subscribe((v) => (velocity.current = v))
+    }, [])
 
     useFrame(() => {
-      api.velocity.set(0, 0, 1)
+      mesh.current.getWorldPosition(camera.position)
       mesh.current.getWorldPosition(ref.current.position)
+      camera.position.z -= 4
       api.velocity.set(direction.x, velocity.current[1], direction.z)
-      // if (jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05) api.velocity.set(velocity.current[0], 20, velocity.current[2])
       ref.current.position.y -= 1
     })
 
-    // useEffect(() => {
-    //   document.addEventListener('keydown', function (event) {
-    //     console.log(
-    //       `Key: ${event.key} with keycode ${event.keyCode} has been pressed`
-    //     )
-    //     switch (event.key) {
-    //       case 'w': {
-    //         ref.current.position.z += 0.04
-    //         setCurrentAction('walk')
-    //         break
-    //       }
-    //       case 'a': {
-    //         ref.current.position.x += 0.04
-    //         break
-    //       }
-    //       case 's': {
-    //         ref.current.position.z -= 0.04
-    //         setCurrentAction('idle')
-    //         break
-    //       }
-    //       case 'd': {
-    //         ref.current.position.x -= 0.04
-    //         break
-    //       }
-    //     }
-    //   })
-
-    //   if (personGuiPanel && personGuiPanel) {
-    //     personGuiPanel
-    //       .add(ref.current.position, 'x')
-    //       .min(0)
-    //       .max(5)
-    //       .step(0.1)
-    //       .name('x')
-    //     personGuiPanel
-    //       .add(ref.current.position, 'y')
-    //       .min(0)
-    //       .max(5)
-    //       .step(0.1)
-    //       .name('y')
-    //     personGuiPanel
-    //       .add(ref.current.position, 'z')
-    //       .min(0)
-    //       .max(5)
-    //       .step(0.05)
-    //       .name('z')
-    //     personGuiPanel.add(ref.current, 'visible')
-    //   }
-    // }, [])
+    useEffect(() => {
+      if (personGuiPanel && personGuiPanel) {
+        personGuiPanel
+          .add(ref.current.position, 'x')
+          .min(0)
+          .max(5)
+          .step(0.1)
+          .name('x')
+        personGuiPanel
+          .add(ref.current.position, 'y')
+          .min(0)
+          .max(5)
+          .step(0.1)
+          .name('y')
+        personGuiPanel
+          .add(ref.current.position, 'z')
+          .min(0)
+          .max(5)
+          .step(0.05)
+          .name('z')
+        personGuiPanel.add(ref.current, 'visible')
+      }
+    }, [])
 
     return (
       <mesh>
         <mesh ref={mesh}></mesh>
         <primitive ref={ref} object={gltf.scene} dispose={null} />
       </mesh>
-    )
-  }
-
-  function PersonCamera() {
-    const ref = useRef()
-    useEffect(() => {
-      if (cameraGuiPanel) {
-        cameraGuiPanel
-          .add(ref.current.position, 'x')
-          .min(0)
-          .max(5)
-          .step(0.1)
-          .name('x')
-        cameraGuiPanel
-          .add(ref.current.position, 'y')
-          .min(0)
-          .max(5)
-          .step(0.1)
-          .name('y')
-        cameraGuiPanel
-          .add(ref.current.position, 'z')
-          .min(-5)
-          .max(5)
-          .step(0.05)
-          .name('z')
-      }
-    })
-    return (
-      <PerspectiveCamera
-        fov={45}
-        ref={ref}
-        position={[0, 2, -5]}
-        makeDefault // Registers it as the default camera system-wide (default=false)
-      ></PerspectiveCamera>
     )
   }
 
@@ -188,18 +163,13 @@ const Model = () => {
         showPanel={0} // Start-up panel (default=0)
         className="stats" // Optional className to add to the stats container dom element
       />
-      <PersonCamera />
-      <OrbitControls enablePan enableZoom enableRotate />
       <ambientLight />
-      <axesHelper args={[5]} />
-      {/* <Plane /> */}
+      <axesHelper args={[100]} />
       <Physics>
-        <Debug>
-          <Plane />
-          <Suspense fallback={null}>
-            <Model />
-          </Suspense>
-        </Debug>
+        <Plane />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
       </Physics>
     </Canvas>
   )
