f8e542b923b4fe7c83fd379a564db57ed3cadb26
raghav-wd <raghav.gupta.gpt@gmail.com>
2021-11-15 05:28:12 +0530

    Enhancement: Add styling to page

--- src/components/Pages/Skills.jsx
+++ src/components/Pages/Skills.jsx
@@ -54,6 +54,45 @@ const Skills = ({ page }) => {
                 <li>React Native, Three.js/react-three/fiber, GraphQL</li>
               </ul>
             </div>
+            <div className="jumbo">Projects</div>
+            <div className="featured-text">A peek on my works</div>
+            <div className="card-container">
+              <div className="card">
+                <h1 className="title">Skima</h1>
+                <p className="text">
+                  Revamped college&apos;s academic website to a more UI
+                  intensive and analytical PW. Available as an installable web
+                  app on android, ios, windows and macOS.
+                </p>
+                <div className="label">React.js</div>
+                <div className="label">Material</div>
+              </div>
+              <div className="card">
+                <h1 className="title">Musifire</h1>
+                <p className="text">
+                  Musicplayer app on android devices, smoothly fetches
+                  lyrics/track info/album art and saves it.
+                </p>
+                <div className="label">Android</div>
+                <div className="label">Php</div>
+              </div>
+            </div>
+            <div className="card-container">
+              <div className="card">
+                <h1 className="title">FoodDude</h1>
+                <p className="text">
+                  A mini-startup, I invented when I came across a problem during
+                  my first year in my college hostel. As I was used to being
+                  sleeping late at night, I always had to deal with the urge of
+                  eating snacks at night but our hostel didn&apos;t have any
+                  facility of canteen at night. So I took it upon me to run an
+                  experiment to start a wholesome business. The main idea was to
+                  develop a software to manage sales of the snacks.
+                </p>
+                <div className="label">Android</div>
+                <div className="label">Php</div>
+              </div>
+            </div>
           </Trail>
         </div>
       </div>
